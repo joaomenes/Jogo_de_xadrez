a62c6e355932ad4cd47fcd7a1bff082806492b24
Joao Menes <joao.menes@hotmail.com>
2021-11-21 12:11:07 -0300

    Jogo de xadrez

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import tabuleiro.Positicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Positicao pos = new Positicao(3, 5);
+		
+		System.out.println(pos);
+
+	}
+
+}
